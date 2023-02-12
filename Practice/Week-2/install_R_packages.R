5757f99d4db66828b4a5af1890deeff33e46a430
Bev Wilson <70167257+bw6xs@users.noreply.github.com>
2023-02-11 20:51:38 -0500

    Initial commit

---
+++ Practice/Week-2/install_R_packages.R
@@ -0,0 +1,12 @@
+
+options(repos=c(CRAN="https://mirrors.nics.utk.edu/cran/")) 
+
+install.packages("tidyverse")
+install.packages("jsonlite")
+install.packages("RSocrata")
+install.packages("janitor")
+install.packages("lubridate")
+install.packages("devtools")
+devtools::install_github("hadley/emo")
+
+
